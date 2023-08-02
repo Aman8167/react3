f1b3725adee672060149fd4813ca914f3405e7df
Aman8167 <118344734+Aman8167@users.noreply.github.com>
2023-08-02 15:22:47 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,50 @@
+
+
+import "./App.css";
+
+function App() {
+  return (
+   <div className="App">
+    <div className="sidebar_container">
+      
+    </div>
+        <div className="body_container">
+             <div className="sum_group">
+              <div className="sum_group2">
+                  <h7>Title -<br></br><br></br>
+                    Description-<br></br><br></br>
+                    Applicable booking-<br></br><br></br>
+                    
+
+
+                   Discount-<br></br>
+                  </h7>
+              </div>
+              <div className="sum_group3">
+                <button id="button">Edit Top Service</button>
+              </div>
+             </div>
+
+           <br></br><br></br>
+           <div className="sum_group">
+              <div className="sum_group2">
+                  <h7>Title -<br></br><br></br>
+                    Description-<br></br><br></br>
+                    Applicable booking-<br></br><br></br>
+                    
+
+
+                   Discount-<br></br>
+                  </h7>
+              </div>
+              <div className="sum_group3">
+                <button id="button">Edit Top Service</button>
+              </div>
+             </div>
+        </div>
+        
+   </div>
+  );
+}
+
+export default App;
